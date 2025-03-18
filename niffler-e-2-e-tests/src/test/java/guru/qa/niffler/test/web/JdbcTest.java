47f09dbd860bf283ba17a413d4d0fec4ddd1b2fa
Almi0312 <almi0312@yandex.ru>
2025-03-18 21:17:10 +0300

    homework 5.2 покрытие тестами

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/JdbcTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/JdbcTest.java
@@ -3,8 +3,8 @@ package guru.qa.niffler.test.web;
 import guru.qa.niffler.data.entity.auth.AuthUserEntity;
 import guru.qa.niffler.model.*;
 import guru.qa.niffler.service.auth.AuthUserDBClient;
+import guru.qa.niffler.service.spend.CategoryDBClient;
 import guru.qa.niffler.service.userdata.UserdataDBClient;
-import guru.qa.niffler.service.spend.CategoryDBSpringClient;
 import guru.qa.niffler.service.spend.SpendDBClient;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
@@ -24,6 +24,7 @@ class JdbcTest {
         String categoryName = "top cource";
         String spendDescription = "cool cource";
         SpendDBClient dbSpend = new SpendDBClient();
+        CategoryDBClient categoryDbClient = new CategoryDBClient();
         dbSpend.findByUsernameAndDescription(MAIN_USERNAME, spendDescription)
                 .ifPresent(entity -> {
                     dbSpend.delete(entity);
@@ -52,18 +53,21 @@ class JdbcTest {
                 "Объект %s неправильно создан. В БД такой %s".formatted(newSpendJson, resultSpend));
         Assertions.assertEquals(newSpendJson.category().name(), resultSpend.category().name(),
                 "%s для %s не равны".formatted(newSpendJson.category(), resultSpend.category()));
+        CategoryJson categoryJson = categoryDbClient.findById(newSpendJson.category().id()).get();
+        Assertions.assertEquals(newSpendJson.category(), categoryJson,
+                "объект %s не равен %s".formatted(newSpendJson.category(), categoryJson));
     }
 
     @Test
     void checkCRUDCategoryTest() {
         String categoryName = "Top category";
-        CategoryDBSpringClient dbSpringClient = new CategoryDBSpringClient();
+        CategoryDBClient dbSpringClient = new CategoryDBClient();
         dbSpringClient.findByUsernameAndName(MAIN_USERNAME, categoryName)
                 .ifPresent(category -> {
                     dbSpringClient.delete(category);
                     log.info("CATEGORY DELETED");
                 });
-        CategoryJson newCategoryJson = dbSpringClient.create(
+        CategoryJson newCategoryJson = dbSpringClient.createCategory(
                 new CategoryJson(
                         null,
                         categoryName,
@@ -117,6 +121,42 @@ class JdbcTest {
         Assertions.assertNull(userJson, "Юзер %s не удален".formatted(userJson));
         authUserEntity = authUserDBClient.findUserByUsername(userdataUserJson.username()).orElse(null);
         Assertions.assertNull(authUserEntity, "Юзер авторизации %s был создан".formatted(authUserEntity));
+    }
+
+    @Test
+    void checkTransactionSpendTest() {
+        String categoryName = "top cource transac2";
+        String spendDescription = "cool cource transac";
+        SpendDBClient dbSpend = new SpendDBClient();
+        CategoryDBClient categoryDbClient = new CategoryDBClient();
+        dbSpend.findByUsernameAndDescription(MAIN_USERNAME, spendDescription)
+                .ifPresent(entity -> {
+                    dbSpend.delete(entity);
+                    log.info("SPEND DELETED");
+                });
+        Assertions.assertThrows(RuntimeException.class,
+                () -> dbSpend.create(new SpendJson(
+                        null,
+                        new Date(),
+                        new CategoryJson(null,
+                                categoryName,
+                                MAIN_USERNAME,
+                                true),
+                        null, // падение транзакции
+                        100.0,
+                        spendDescription,
+                        MAIN_USERNAME
+                )));
+        log.info("CHECK NOT SPEND CREATED");
+        SpendJson resultSpend = dbSpend
+                .findByUsernameAndDescription(MAIN_USERNAME, spendDescription).orElse(null);
+        Assertions.assertNull(resultSpend,
+                "объект %s был создан(".formatted(resultSpend));
+        log.info("CHECK NOT CATEGORY CREATED");
+        CategoryJson categoryJson = categoryDbClient
+                .findByUsernameAndName(MAIN_USERNAME, categoryName).orElse(null);
+        Assertions.assertNull(categoryJson,
+                "объект %s был создан(".formatted(categoryJson));
     }
 
     @Test
