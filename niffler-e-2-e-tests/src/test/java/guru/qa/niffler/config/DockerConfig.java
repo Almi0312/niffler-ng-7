c67bc35bccddc634617ef9b873520f4eb21f1821
Almi0312 <almi0312@yandex.ru>
2025-04-21 17:44:58 +0300

    stream 7.1 Добавление микро фабрики для конфига для докера

---
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/config/DockerConfig.java
@@ -0,0 +1,58 @@
+package guru.qa.niffler.config;
+
+import javax.annotation.Nonnull;
+
+enum DockerConfig implements Config {
+  INSTANCE;
+
+  @Override
+  public @Nonnull String frontUrl() {
+    return "";
+  }
+
+  @Override
+  public @Nonnull String authJdbcUrl() {
+    return "";
+  }
+
+  @Override
+  public @Nonnull String authUrl() {
+      return "";
+  }
+
+  @Override
+  public @Nonnull String spendUrl() {
+    return "";
+  }
+
+  @Override
+  public @Nonnull String spendJdbcUrl() {
+    return "";
+  }
+
+  @Override
+  public @Nonnull String currencyJdbcUrl() {
+    return "";
+  }
+
+  @Override
+  public @Nonnull String ghUrl() {
+    return "";
+  }
+
+  @Override
+  public @Nonnull String userdataJdbcUrl() {
+    return "";
+  }
+
+  @Override
+  public @Nonnull String userdataUrl() {
+      return "";
+  }
+
+  @Override
+  public @Nonnull String gatewayUrl() {
+    return "";
+  }
+
+}
