847133c529d0681e564b5df9d8d4ad982d2c538d
Almi0312 <almi0312@yandex.ru>
2025-04-23 08:39:33 +0300

    homework 7.1 создание компонентов

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/page/component/Header.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/page/component/Header.java
@@ -1,10 +1,80 @@
 package guru.qa.niffler.page.component;
 
+import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
+import guru.qa.niffler.page.EditSpendingPage;
+import guru.qa.niffler.page.LoginPage;
+import guru.qa.niffler.page.MainPage;
+import guru.qa.niffler.page.profileInfo.AllPeoplePage;
+import guru.qa.niffler.page.profileInfo.FriendsPage;
+import guru.qa.niffler.page.profileInfo.ProfilePage;
+import io.qameta.allure.Step;
 
-import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
+import static java.lang.String.format;
 
-public class Header {
-    private final SelenideElement self = $("#root header");
+public class Header extends BaseComponent {
+
+    public Header() {
+        super($("#root header"));
+    }
+
+    @Step("Перейти на страницу друзей пользователя")
+    public FriendsPage toFriendsPage() {
+        String namePage = "Friends";
+        getBtnMenu().shouldBe(visible).click();
+        getMenuItems().find(text(namePage)).click();
+        return new FriendsPage();
+    }
+
+    @Step("Перейти на страницу со всеми пользователями")
+    public AllPeoplePage toAllPeoplePage() {
+        String namePage = "All People";
+        getBtnMenu().shouldBe(visible).click();
+        getMenuItems().find(text(namePage)).click();
+        return new AllPeoplePage();
+    }
+
+    @Step("Перейти на страницу профиля пользователя")
+    public ProfilePage toProfilePage() {
+        String namePage = "Profile";
+        getBtnMenu().shouldBe(visible).click();
+        getMenuItems().find(text(namePage)).click();
+        return new ProfilePage();
+    }
+
+    @Step("Выйти из учетной записи")
+    public LoginPage signOut() {
+        String namePage = "Sign out";
+        getBtnMenu().shouldBe(visible).click();
+        getMenuItems().find(text(namePage)).click();
+        return new LoginPage();
+    }
+
+    @Step("Перейти на страницу добавления расхода(spending)")
+    public EditSpendingPage addSpendingPage() {
+        link("New spending").shouldBe(visible).click();
+        return new EditSpendingPage();
+    }
+
+    @Step("Перейти на главную страницу")
+    public MainPage toMainPage() {
+        logo().shouldBe(visible).click();
+        return new MainPage();
+    }
+
+    protected final SelenideElement getBtnMenu() {
+        return self.$x(".//button[@aria-label='Menu']");
+    }
+
+    protected final ElementsCollection getMenuItems() {
+        return $$x(".//ul[@role='menu']/li//a");
+    }
+
+    protected SelenideElement logo() {
+        return $x(".//a[./h1[normalize-space(text()) = 'Niffler']]");
+    }
 
 }
