9b2ea01d6740d1b960cb6a78027029f3d680043c
Almi0312 <almi0312@yandex.ru>
2025-04-29 09:49:59 +0300

    stream 8.2 тесты для проверки многопоточности

---
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/fake/SecondTest.java
@@ -0,0 +1,16 @@
+package guru.qa.niffler.test.fake;
+
+import org.junit.jupiter.api.Test;
+
+public class SecondTest {
+
+    @Test
+    void secondTestClassMethod0() throws InterruptedException {
+        Thread.sleep(1000);
+    }
+
+    @Test
+    void secondTestClassMethod1() throws InterruptedException {
+        Thread.sleep(1000);
+    }
+}
