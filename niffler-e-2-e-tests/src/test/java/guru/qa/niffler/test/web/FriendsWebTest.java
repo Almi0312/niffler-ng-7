5cfb584d80050c5467d40a11895002965c5746e5
Almi0312 <almi0312@yandex.ru>
2025-05-10 12:51:06 +0300

    homework 11.2 смена на apiLogin в тестах

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/FriendsWebTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/FriendsWebTest.java
@@ -1,85 +1,75 @@
 package guru.qa.niffler.test.web;
 
 import guru.qa.niffler.config.Config;
+import guru.qa.niffler.jupiter.annotation.ApiLogin;
 import guru.qa.niffler.jupiter.annotation.User;
 import guru.qa.niffler.jupiter.annotation.meta.WebTest;
 import guru.qa.niffler.model.rest.UserdataUserJson;
-import guru.qa.niffler.page.LoginPage;
+import guru.qa.niffler.page.profileInfo.AllPeoplePage;
+import guru.qa.niffler.page.profileInfo.FriendsPage;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.open;
+import static guru.qa.niffler.config.Constants.DEFAULT_PASSWORD;
 
 @WebTest
 public class FriendsWebTest {
     private static final Config CFG = Config.getInstance();
 
+    @Test
+    @ApiLogin
     @User(username = "wolf_with_friends",
             friends = 1)
-    @Test
     void friendShouldBePresentInFriendsTable(UserdataUserJson user) {
-        open(CFG.frontUrl(), LoginPage.class)
-                .login(user.username(), user.testData().password())
-                .getHeader()
-                .toFriendsPage()
+        open(FriendsPage.URL, FriendsPage.class)
                 .checkFriendInTable(user.testData().friends().getFirst().username());
     }
 
-    @User(username = "lion_empty")
     @Test
+    @ApiLogin(username = "lion_empty", password = DEFAULT_PASSWORD)
     void friendTableShouldBeEmptyForNewUser(UserdataUserJson user) {
-        open(CFG.frontUrl(), LoginPage.class)
-                .login(user.username(), user.testData().password())
-                .getHeader()
-                .toFriendsPage()
+        open(FriendsPage.URL, FriendsPage.class)
                 .checkSelectedTabByName("Friends")
                 .checkEmptyTable();
     }
 
+    @Test
+    @ApiLogin
     @User(username = "circus_with_income",
             incomeInvitations = 1)
-    @Test
     void incomeInvitationBePresentInFriendsTable(UserdataUserJson user) {
-        open(CFG.frontUrl(), LoginPage.class)
-                .login(user.username(), user.testData().password())
-                .getHeader()
-                .toFriendsPage()
+        open(FriendsPage.URL, FriendsPage.class)
                 .checkSelectedTabByName("Friends")
                 .checkIncomeInTable(user.testData().income().getFirst().username());
     }
 
+    @Test
+    @ApiLogin
     @User(username = "cat_with_outcome",
             outcomeInvitations = 1)
-    @Test
     void outcomeInvitationBePresentInAllPeoplesTable(UserdataUserJson user) {
-        open(CFG.frontUrl(), LoginPage.class)
-                .login(user.username(), user.testData().password())
-                .getHeader()
-                .toAllPeoplePage()
+        open(AllPeoplePage.URL, AllPeoplePage.class)
                 .checkOutcomeInTable(user.testData().outcome().getFirst().username());
     }
 
-    @User(incomeInvitations = 1)
     @Test
+    @ApiLogin
+    @User(incomeInvitations = 1)
     void AcceptOutcomeInvitation(UserdataUserJson user) {
         String incomeUsername = user.testData().income().getFirst().username();
-        open(CFG.frontUrl(), LoginPage.class)
-                .login(user.username(), user.testData().password())
-                .getHeader()
-                .toFriendsPage()
+        open(FriendsPage.URL, FriendsPage.class)
                 .checkSelectedTabByName("Friends")
                 .checkIncomeInTable(incomeUsername)
                 .acceptIncomeInvication(incomeUsername)
                 .checkFriendInTable(incomeUsername);
     }
 
-    @User(incomeInvitations = 1)
     @Test
+    @ApiLogin
+    @User(incomeInvitations = 1)
     void DeclineOutcomeInvitation(UserdataUserJson user) {
         String incomeUsername = user.testData().income().getFirst().username();
-        open(CFG.frontUrl(), LoginPage.class)
-                .login(user.username(), user.testData().password())
-                .getHeader()
-                .toFriendsPage()
+        open(FriendsPage.URL, FriendsPage.class)
                 .checkSelectedTabByName("Friends")
                 .checkIncomeInTable(incomeUsername)
                 .declineIncomeInvication(incomeUsername)
